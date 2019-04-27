bfabe0d163795b8868e414ef0e0b5e5020b13b63
mrpineapple303 <44753433+mrpineapple303@users.noreply.github.com>
2019-04-27 20:23:44 +0700

    Add files via upload

---
+++ Pineapple_303v1.0.sh
@@ -0,0 +1,302 @@
+#!/system/xbin/bash
+clear
+yellow='\033[33;1m'
+green='\033[32;1m'
+red='\033[31;1m'
+echo ""
+clear
+figlet "PineInstaller"|lolcat
+echo "\033[33;1m|====================================|"
+echo "\033[31;1m|+________Author:Pineapple_303_______+"
+echo "\033[32;1m|+___Team:|-L4EFY-| & D35TR0Y SQUAD__+"
+echo "\033[31;1m|+______Contact:085830556606_________+"
+echo "\033[32;1m|+_______Supported:-BUFT- 2E4H_______+"
+echo "\033[31;1m|+===INDONESIAN TERMUX ASSOCIATION===+"
+echo "\033[33;1m|====================================|"
+echo $green
+echo "======================================="|lolcat
+echo "1. admin panel"|lolcat
+echo "======================================="|lolcat
+echo "2. Ping Google"|lolcat
+echo "======================================="|lolcat
+echo "3. Torshammer"|lolcat
+echo "======================================="|lolcat
+echo "4. SQL Map"|lolcat
+echo "======================================="|lolcat
+echo "5. LazyMux"|lolcat
+echo "======================================="|lolcat
+echo "6. TOOLKIT TUAN B4DUT"|lolcat
+echo "======================================="|lolcat
+echo "7. kode Unik Facebook by IY-G00D"|lolcat
+echo "======================================="|lolcat
+echo "8. AUTOSCAN By Mr_Xid"|lolcat
+echo "======================================="|lolcat
+echo "9. INSTALLER BY Mr_Xid"|lolcat
+echo "======================================="|lolcat
+echo "10. LITEDDOS"|lolcat
+echo "======================================="|lolcat
+echo "11. moon-buggy"|lolcat
+echo "======================================="|lolcat
+echo "12. LITESPAM"|lolcat
+echo "======================================="|lolcat
+echo "13. Tools TUANB4DUTv1"|lolcat
+echo "======================================="|lolcat
+echo "14. RED_HAWK"|lolcat
+echo "======================================="|lolcat
+echo "15. Hash-Buster"|lolcat
+echo "======================================="|lolcat
+echo "16. Santet-online"|lolcat
+echo "======================================="|lolcat
+echo "17. LITESCRIPT"|lolcat
+echo "======================================="|lolcat
+echo "18. Tools Mr.Cakil"|lolcat
+echo "======================================="|lolcat
+echo "19. Bot epi-cash by xNot_Found"|lolcat
+echo "======================================="|lolcat
+echo "20. Bot Caping by xNot_Found"|lolcat
+echo "======================================="|lolcat
+echo "21. Bot Yd-news by xNot_Found"|lolcat
+echo "======================================="|lolcat
+echo "22. Bot YogoVideo by xNot_Found"|lolcat
+echo "======================================="|lolcat
+echo "23. Bot Flash-Go by xNot_Found"|lolcat
+echo "======================================="|lolcat
+echo "24. XERXES"|lolcat
+echo "======================================="|lolcat
+echo "25. Perkiraan cuaca"|lolcat
+echo "======================================="|lolcat
+echo "26. EXIT?"|lolcat
+echo "======================================="|lolcat
+read -p "root@Pineapple_303~>" nas
+
+if [ $nas = 1 ] || [ $nas = 1 ]
+then
+clear
+git clone https://github.com/Techzindia/admin_penal
+cd admin_penal
+chmod +x admin_panel_finder.py
+python2 admin_panel_finder.py
+fi
+
+if [ $nas = 2 ] || [ $nas = 2 ]
+then
+clear
+pkg install w3m
+w3m www.google.com
+fi
+
+if [ $nas = 3 ] || [ $nas = 3 ]
+then
+clear
+toilet -f standard -F gay "DDoS"
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer
+cd torshammer
+read -p "Enter target" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $nas = 4 ] || [ $nas = 4 ]
+then
+clear
+toilet -f standard -F gay "SQLMap"
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap
+cd sqlmap
+read -p "enter target" target
+python2 sqlmap.py -D target
+fi
+
+if [ $nas = 5 ] || [ $nas = 5 ]
+then
+clear
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+fi
+
+if [ $nas = 6 ] || [ $nas = 6 ]
+then
+clear
+git clone https://github.com/TUANB4DUT/TOOLKIT
+cd TOOLKIT
+chmod 777 TUANB4DUT.sh
+./TUANB4DUT.sh
+fi
+
+if [ $nas = 7 ] || [ $nas = 7 ]
+then
+clear
+git clone https://github.com/InYourG00D1/Unix
+cd Unix
+sh KD-FB.sh
+fi
+
+if [ $nas = 8 ] || [ $nas = 8 ]
+then
+clear
+git clone https://github.com/Xidploit/AUTOSCAN
+cd AUTOSCAN
+python2 XIDScan.py
+fi
+
+if [ $nas = 9 ] || [ $nas = 9 ]
+then
+clear
+git clone https://github.com/Xidploit/INSTALLERMR_XID
+cd INSTALLERMR_XID
+sh installer0.0.1.sh
+fi
+
+if [ $nas = 10 ] || [ $nas = 10 ]
+then
+clear
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+fi
+
+if [ $nas = 11 ] || [ $nas = 11 ]
+then
+clear
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $nas = 12 ] || [ $nas = 12 ]
+then
+clear
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $nas = 13 ] || [ $nas = 13 ]
+then
+clear
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+fi
+
+if [ $nas = 14 ] || [ $nas = 14 ]
+then
+clear
+git clone https://gitub.com/Tuhinsubhra/REDHAWK
+cd RED_HAWK
+chmod +x rhawk.php
+apt install php
+ls
+php rhawk.php
+fi
+
+if [ $nas = 15 ] || [ $nas = 15 ]
+then
+clear
+git clone https://github.com/ UltimateHackers/Hash-Buster
+cd Hash-Buster
+chmod 777 hash.py
+fi
+
+if [ $nas = 16 ] || [ $nas = 16 ]
+then
+clear
+git clone https://github.com/Gameye98/santet-online
+ls
+cd santet-online
+ls
+python2 santet.py
+fi
+
+if [ $nas = 17 ] || [ $nas = 17 ]
+then
+clear
+git clone https://github.com/4L13199/LITESCRIPT
+ls
+cd LITESCRIPT
+ls
+python2 LITESCRIPT.py
+fi
+
+if [ $nas = 18 ] || [ $nas = 18 ]
+then
+clear
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+fi
+
+if [ $nas = 19 ] || [ $nas = 19 ]
+then
+clear
+pkg install php git
+git clone https://github.com/hatakecnk/Bot-Epicash
+cd Bot-Epicash
+php epicash.php
+fi
+
+if [ $nas = 20 ] || [ $nas = 20 ]
+then
+clear
+pkg install php git
+git clone https://github.com/hatakecnk/bot-caping
+cd bot-caping && php caping.php
+fi
+
+if [ $nas = 21 ] || [ $nas = 21 ]
+then
+clear
+pkg install php git
+git clone https://github.com/hatakecnk/Bot-YDNews
+cd Bot-YDNews
+php ydnews.php
+fi
+
+if [ $nas = 22 ] || [ $nas = 22 ]
+then
+clear
+Tool Bot-YogoVideo*
+pkg install php git nano
+git clone https://github.com/hatakecnk/Bot-YogoVideo
+cd Bot-YogoVideo
+nano config.php
+php yogovideo.php
+fi
+
+if [ $nas = 23 ] || [ $nas = 23 ]
+then
+clear
+pkg install php git nano
+git clone https://github.com/hatakecnk/Bot-FlashGo
+cd Bot-FlashGO
+nano config.php
+php bot-flashgo.php
+fi
+
+if [ $nas = 24 ] || [ $nas = 24 ]
+then
+clear
+pkg install clang
+git clone https://github.com/zanyarjamal
+cd xerxes
+gcc xerxes.c -o xerxes
+ fi
+
+if [ $nas = 25 ] || [ $nas = 25 ]
+then
+clear
+read -p "masukan lokasi mu" lokasi
+curl http://wttr.in/ lokasi
+fi
+
+if [ $nas = 26 ] || [ $nas = 26 ]
+then
+clear
+figlet "See You"
+sleep 3
+clear
+fi
